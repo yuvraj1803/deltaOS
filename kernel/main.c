e2b3cfe4f3c89bc49f6c99972173f73b5f522f8c
yuvraj1803 <ysakshith@gmail.com>
2023-10-26 12:54:20 +0530

    main now initialises heap and irq too

--- kernel/main.c
+++ kernel/main.c
@@ -3,12 +3,16 @@
 #include "stdio.h"
 #include "kernel/misc.h"
 #include "kernel/irq.h"
+#include "mm/mm.h"
 
 void main(){
-    
+
+    heap_init();    
     uart_init();
-    timer_init();
+    irq_init();
     interrupt_controller_init();
+    timer_init();
 
     printf("hello threre!");
+
 }
