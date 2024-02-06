f45cd3a3b18618ebb5f4a7682a80d585c35130b4
yuvraj1803 <ysakshith@gmail.com>
2024-02-06 12:32:53 +0530

    added install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt install qemu-system-aarch64 -y
+mkdir build
+mkdir build/boot
+mkdir build/deltaSSE
+mkdir build/lib
+mkdir build/mm
+mkdir build/shell
+mkdir build/kernel
+mkdir build/drivers
+mkdir build/omegaCOM
+rm -rf deltaSSE
+git clone https://www.github.com/yuvraj1803/deltaSSE.git
+
+
+
